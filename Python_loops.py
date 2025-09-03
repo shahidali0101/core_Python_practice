60ee19fed751c2176ac206b219b9abf0cd7c1166
Shahid Ali <shahids.user@gmail.com>
2025-09-03 23:13:51 +0530

    Update Python_loops.py

--- Python_loops.py
+++ Python_loops.py
@@ -175,6 +175,9 @@ for i in range(40,20,-1):
    if i%2==0:
       print(i)
 
+#---------------------------------
+"""Print numbers from 5 to 25 using a for loop. 
+ If the number is divisible by 7, stop the loop."""
 
 
 
