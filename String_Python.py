2dc2888a21faf214a2535bba662fa742a81f3334
Shahid Ali <shahids.user@gmail.com>
2025-09-28 21:12:40 +0530

    String in python

---
+++ String_Python.py
@@ -0,0 +1,37 @@
+# Python_Strings_Manipulation.py
+"""
+Topic: Strings in Python & String Manipulation
+Author: Amisha Nakoti
+"""
+
+# ---------------------------
+# 1. What is a String?
+# ---------------------------
+# A string is a sequence of characters enclosed in single quotes (' '), double quotes (" "), or triple quotes (''' ''' / """ """).
+
+string1 = 'Hello'
+string2 = "Python"
+string3 = '''This is 
+a multi-line string.'''
+
+print(string1)
+print(string2)
+print(string3)
+
+
+# ---------------------------
+# 2. Accessing Characters in a String
+# ---------------------------
+word = "Python"
+print("First character:", word[0])   # Indexing starts at 0
+print("Last character:", word[-1])  # Negative indexing
+
+
+# ---------------------------
+# 3. String Slicing
+# ---------------------------
+print("Substring [0:4]:", word[0:4])   # Pyth
+print("Substring [:3]:", word[:3])     # Pyt
+print("Substring [2:]:", word[2:])     # thon
+
+
