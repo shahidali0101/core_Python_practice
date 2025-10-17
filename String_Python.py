d693dfdde1e33dd25f7e90a6fab7d5a2d8ca2120
Shahid Ali <shahids.user@gmail.com>
2025-10-18 00:11:49 +0530

    Update String_Python.py

--- String_Python.py
+++ String_Python.py
@@ -156,6 +156,16 @@ if x.startswith("a") and x.endswith("a"):
 else:
     print("no") 
 
+'''   replece i with this "*" '''
+x="India is a Incredible country".lower()
+y=x.replace("i","*")
+print(y)
+
+''' replece i with this "*" and rest of it print in capital'''
+
+x=input("enter word:").lower()
+y=x.replace("i","*")
+print(y.upper())
 
 
 
