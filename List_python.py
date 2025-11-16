cabcc45c14afb3a180330eccfc9944569367aaae
Shahid Ali <shahids.user@gmail.com>
2025-11-16 15:24:46 +0530

    List

---
+++ List_python.py
@@ -0,0 +1,29 @@
+'''_____________________________ LIST __________________________________________________________
+list is part of sequence data type which is a collection of repitative elements
+it is used to store multiple data in single variable and its always written in square bracket
+_________________________________________________________________________________________________
+
+1-list are written in square bracket.
+2-list are mutable or changable.
+3-list are used to store multiple values in a single variable.
+4-list are ordered.
+5-list allowed duplicate value.
+6-list are indexed.
+7-list support Multiple type of data  
+                          
+                        ----------------------------------------
+                        #how to create a blank list  
+                          x=[]
+                          print(type(x))     #class list
+                          print(len(x))      # 0
+                          
+                          x=list()
+                          print(x)
+                        ----------------------------------------
+   ------------------------------------------------------------------------------------------
+   0-list consider " , "comma in last✅ but in begining or mid it will show error❌                      
+---------------------------------------------------------------------------------------------------
+INDEXING - Iindexing are used to Extract the single elements from list.
+SLICING  - Slicing are used to extract the range of elements from list.
+---------------------------------------------------------------------------------------------------
+______________________________________________________________________________________________________'''
