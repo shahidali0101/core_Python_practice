6259c91d8d4736823635117ee59b0e8d808002e8
Shahid Ali <shahids.user@gmail.com>
2025-12-02 21:01:25 +0530

    Update List_python.py

--- List_python.py
+++ List_python.py
@@ -1,7 +1,7 @@
 '''_____________________________ LIST __________________________________________________________
 list is part of sequence data type which is a collection of repitative elements
 it is used to store multiple data in single variable and its always written in square bracket
-________________________________________________________________________________________________
+________________________________________________________________________________________________ 
 
 1-list are written in square bracket.
 2-list are mutable or changable.
@@ -255,6 +255,7 @@ print(y)
 SORT--its arrange the data in ascending or descending order in orginal list
 SORTED---its also arrange data in ascending or descending order but it store in new variable.
 _________________________________________________________________________________________________'''
+
 
 
 
