760c351de8f5f5e86358376aca2bfab9f0cc7d25
Shahid Ali <shahids.user@gmail.com>
2026-01-19 20:33:30 +0530

    Dict

---
+++ Dictionaries.py
@@ -0,0 +1,35 @@
+'''_________________________________||  DICTIONARY   ||__________________________________________________'''
+
+'''DICTIONARY - Dictionary is a collection of non repitative elements it is always written with the pair of 
+                keys and values .
+               and it also store multiple elements in dict.
+               written in curly brackets.
+
+1->  written in curly brackets with part of keys and values. 
+2->  dict are ordered .
+3->  dict are mutable or changable .
+4->  dict are not indexed .   
+5->  dict not allow duplicate keys .
+6-.  it also support multiple typpe of Data.  
+_________________________________________________________________________________________________________
+
+GET-->  its extract values base of Keys from dict.
+kEY-->  Its return array where show entire keys of dictionary.
+VALUES-> Its return arry where show entire values from dict.
+_________________________________________________________________________________________________________'''
+
+''' how to create a dict.'''
+# x={}
+# x=dict()
+# print(type(x))
+#-----------------------------------------------
+
+
+# D={"Brand":"TATA",
+#    "Model":"xyz",
+#    "Color":"Black",
+#    "year":2000}
+
+# print(D)
+# print(type(D))    #dict
+# print(len(D))     #4
