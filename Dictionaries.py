da3a41f16ec881a9375383fc0b97093c82553cbb
Shahid Ali <shahids.user@gmail.com>
2026-03-08 23:14:02 +0530

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -4,7 +4,7 @@
                 keys and values .
                and it also store multiple elements in dict.
                written in curly brackets.
-
+ 
 1->  written in curly brackets with part of keys and values. 
 2->  dict are ordered .
 3->  dict are mutable or changable . 
@@ -49,3 +49,4 @@ x=[i for i in m if "o" in i]
 for i in x:
     del m[i]
 print(m)
+
